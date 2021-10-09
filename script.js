51052bad1bd6cc34db8f1746a9d06e24bc755f76
Thomas Tahmassebi <tjtahmas@gmail.com>
2021-10-08 23:54:48 -0400

    init commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// TJ Tahmassebi's Code
+// 10/8: Started making questions. Figure out how to make array of objects (possibly JSON)
+
+var Question1 = {
+    Q: "_____ require a comma after each new variable.",
+    As: ["Arrays","Objects","Strings","Lines"],
+    Correct: 2,
+}
+
+var Question2 = {
+    Q: "A common name for a javascript file is...",
+    As: ["style","index","README","script"],
+    Correct: 4,
+}
+
+var Question3 = {
+    Q: "What does API stand far?",
+    As: ["Airwaves Perpetrator Imposition", "Alturistic Polygraph Induction", "Applitcation Programming Interface", "Antinodal Plyometric Interference"],
+    Correct: 3,
+}
+
+var Question4 = {
+    Q: "What do you put before a comment in JavaScript?",
+    As: ["<!--","##","/*","//"],
+    Correct: 4,
+}

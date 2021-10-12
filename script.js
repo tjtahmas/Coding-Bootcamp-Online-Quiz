a4ff7d70b4097e9c7f98167ba599fdf0766fbcff
Thomas Tahmassebi <tjtahmas@gmail.com>
2021-10-12 02:24:00 -0400

    Added jQuery link, event listeners, keeps track of answers, displays score, stops when timer goes off

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // TJ Tahmassebi's Code
 // 10/8: Started making questions. Figure out how to make array of objects (possibly JSON)
 // 10/10: Added timer and more questions. Added elements to HTML and created basic CSS styling. Stores score and initials to local storage. Displays first question and options
+// 10/11: Added jQuery link to add multiple even listeners to buttons. Keeps track of answers. Displays score at the end. Stops when timer goes off. 
+
+//Still need to add initials, make it loop. Maybe make a start button
 
 var score = [0, 0]; //Keeps track of their correct and incorrect answers. First value is correct, second is incorrect
 
 var Question1 = {
     Q: "_____ require a comma after each new variable.",
-    As: ["Arrays","Objects","Strings","Lines"],
+    As: ["Arrays", "Objects", "Strings", "Lines"],
     Correct: 2,
 }
 
 var Question2 = {
     Q: "A common name for a javascript file is...",
-    As: ["style","index","README","script"],
+    As: ["style", "index", "README", "script"],
     Correct: 4,
 }
 
@@ -24,90 +27,158 @@ var Question3 = {
 
 var Question4 = {
     Q: "What do you put before a comment in JavaScript?",
-    As: ["<!--","##","/*","//"],
+    As: ["<!--", "##", "/*", "//"],
     Correct: 4,
 }
 
 var Question5 = {
     Q: "Which logical operator compares value and type?",
-    As: ["=","==","===","%"],
+    As: ["=", "==", "===", "%"],
     Correct: 3,
 }
 
 var Question6 = {
     Q: "Which logical operator means 'or'?",
-    As: ["@@","&&","##","||"],
+    As: ["@@", "&&", "##", "||"],
     Correct: 4,
 }
 
 var Question7 = {
     Q: "Which response means 'An assigned variable with no value'?",
-    As: ["null","undefined","false","0"],
+    As: ["null", "undefined", "false", "0"],
     Correct: 1,
 }
 
 var Question8 = {
     Q: "An array's first index is assigned which value?",
-    As: ["1","0","i","true"],
+    As: ["1", "0", "i", "true"],
     Correct: 2,
 }
 
 var Question9 = {
     Q: "Which method adds the value as the first element of an arry without removing any elements?",
-    As: ["concat","unshift","pop","toUpperCase"],
+    As: ["concat", "unshift", "pop", "toUpperCase"],
     Correct: 2,
 }
 
 var Question10 = {
     Q: "Which statement creates a loops that continues to run as long as a specified condition is true?",
-    As: ["while","if","var","for"],
+    As: ["while", "if", "var", "for"],
     Correct: 1,
 }
 
-//Make Array of Question Objects
+var options = $('.options');
+    options.on('click', '.option', chooseSelection);
 
-//var Quiz = JSON.stringify(Question1,Question2,Question3,Question4,Question5,Question6,Question7,Question8,Question9,Question10);
-//console.log(JSON.parse(Quiz))
+//Displays Question and Options from Object entered
+function displayQuestion(question) {
+    //Display Question
+    var display = document.getElementById("display");
+    display.textContent = question.Q;
 
-//Spawn in Question elements to correct attributes while the timer runs
+    //Get Answers
+    var first = document.getElementById("Q1");
+    var second = document.getElementById("Q2");
+    var third = document.getElementById("Q3");
+    var fourth = document.getElementById("Q4");
 
-//Question
-var question = document.getElementById("display");
-question.textContent = Question1.Q;
+    //Set answers to options
+    first.textContent = question.As[0];
+    second.textContent = question.As[1];
+    third.textContent = question.As[2];
+    fourth.textContent = question.As[3];
+}
 
+var Key = [2,4,3,4,3,4,1,2,2,1];
+var qNumber = 0;
 
-//Answer Options
-var first = document.getElementById("Q1");
-var second = document.getElementById("Q2");
-var third = document.getElementById("Q3");
-var fourth = document.getElementById("Q4");
+function chooseSelection(event){
+    var chosen = event.target.value;
+    if(chosen == Key[qNumber - 1]){ //Keep track of the correct and incorrect answers
+        score[0]++;
+        nextQuestion();
+    } else {
+        score[1]++;
+        nextQuestion();
+    }
+    //console.log(score)
+    //console.log(Key[qNumber])
+}
 
-console.log(first)
-console.log(Question1.As[0])
+function selectQuestion(qNumber){ //Spawn in Question elements to correct attributes while the timer runs
+    if (qNumber == 1){
+        displayQuestion(Question1);
+    } else if (qNumber == 2){
+        displayQuestion(Question2);
+    } else if (qNumber == 3){
+        displayQuestion(Question3);
+    } else if (qNumber == 4){
+        displayQuestion(Question4);
+    } else if (qNumber == 5){
+        displayQuestion(Question5);
+    } else if (qNumber == 6){
+        displayQuestion(Question6);
+    } else if (qNumber == 7){
+        displayQuestion(Question7);
+    } else if (qNumber == 8){
+        displayQuestion(Question8);
+    } else if (qNumber == 9){
+        displayQuestion(Question9);
+    } else if (qNumber == 10){
+        displayQuestion(Question10);
+    }  
+    else {
+        clearOptions();
+        timerEl.textContent = '0';
+        showScore();
+    }
+}
 
-first.textContent = Question1.As[0];
-second.textContent = Question1.As[1];
-third.textContent = Question1.As[2];
-fourth.textContent = Question1.As[3];
+function clearOptions(){
+    var Q = $('.question');
+    Q.empty();
+    var O = $('.option')
+    O.parent().empty();
+    spawnTextbox();
+}
 
-//Turn the above into a function, too much repeated action
+function showScore(){ //Display score at the end
+    var show = $('#scoreboard')
+    show.text("You got " + score[0] + " correct and " + score[1] + " wrong.");
+}
 
-//Add event listener to each list item
+function nextQuestion (){ 
+    qNumber++; 
+    selectQuestion(qNumber);
+}
 
-
-
-
-
-//Keep track of the correct and incorrect answers
-
-//Display score at the end
-
+nextQuestion();
+countdown();
 
 //Save score and initials to local storage
-initials = "TT";
+function spawnTextbox(){
+    var div = $('.inits');
+    var input = $('<input>')
+    var message = $('<h3>')
+    var button = $('<button>')
+    //button.addEventListener("click", enterInitials);
+    message.text('Enter your initials!')
+    div.append(message);
+    div.append(input);
+    div.append(button);
+    
+}
 
-localStorage.setItem("#score",score[0]);
-localStorage.setItem("#initials",initials);
+function enterInitials(){
+    var div = $('.inits');
+    var input = div.children().eq(1);
+    initials = input.value;
+
+    localStorage.setItem("#score", score[0].toString());
+    localStorage.setItem("#initials", initials);
+}
+
+
 
 
 
@@ -117,19 +188,22 @@ localStorage.setItem("#initials",initials);
 var timerEl = document.getElementById('countdown');
 
 function countdown() {
-  var timeLeft = 30;
+    var timeLeft = 60;
 
-  //
-  var timeInterval = setInterval(function () {
-    timeLeft--;
-    timerEl.textContent = timeLeft + " seconds left";
+    //
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = timeLeft + " seconds left";
 
-    if(timeLeft == 0){
-      clearInterval(timeInterval);
-      displayMessage();
-    }
-  }, 1000);
+        if (timeLeft == 0) {
+            clearInterval(timeInterval);
+            if (qNumber !== 11) {
+            clearOptions();
+            showScore();
+            }
+        }
+    }, 1000);
 }
 
-countdown();
+
 
